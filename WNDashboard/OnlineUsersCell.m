6b87fa01454e2a1297858cc870a8105d59c48fc5
Jan Franz Palngipang <franz.palngipang@gmail.com>
2015-06-12 16:37:03 +0800

    Added ustom table cell for online users

---
+++ WNDashboard/OnlineUsersCell.m
@@ -0,0 +1,23 @@
+//
+//  OnlineUsersCell.m
+//  WNDashboard
+//
+//  Created by Jan Franz Palngipang on 6/12/15.
+//  Copyright (c) 2015 Jan Franz Palngipang. All rights reserved.
+//
+
+#import "OnlineUsersCell.h"
+
+@implementation OnlineUsersCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ WNDashboard/SurveyCell.m
@@ -0,0 +1,23 @@
+//
+//  SurveyCell.m
+//  WNDashboard
+//
+//  Created by Jan Franz Palngipang on 6/12/15.
+//  Copyright (c) 2015 Jan Franz Palngipang. All rights reserved.
+//
+
+#import "SurveyCell.h"
+
+@implementation SurveyCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
